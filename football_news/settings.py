9f25a0b4a2419a97256e28524241bec48de7b801
AvilioWatson <aviliowatson@gmail.com>
2025-09-10 19:16:55 +0700

    super final final

--- football_news/settings.py
+++ football_news/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "wildan-al41-footballnews.pbp.cs.ui.ac.id"]
 CSRF_TRUSTED_ORIGINS = [
-    "https://wildan-al41-footballnews.pbp.cs.ui.ac.id/"
+    "https://wildan-al41-footballnews.pbp.cs.ui.ac.id"
 ]
 
 
